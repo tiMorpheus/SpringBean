1539015913526bb91b03cbaac0ba433ba8961f02
Tima Tolochko <timur10645@icloud.com>
2016-10-04 17:52:13 +0300

    Bean scopes annotation

--- SpringBeanScopesExample/src/main/java/github/tiMorpheus/customer/services/CustomerService.java
+++ SpringBeanScopesExample/src/main/java/github/tiMorpheus/customer/services/CustomerService.java
@@ -1,5 +1,11 @@
 package github.tiMorpheus.customer.services;
 
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Service;
+
+@Service
+@Scope("prototype")
 public class CustomerService {
 
     private String message;
