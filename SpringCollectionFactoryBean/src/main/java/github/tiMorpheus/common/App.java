a4ea9282bdb74a993ec5d91673ae8c92cb4b0a7f
Tima Tolochko <timur10645@icloud.com>
2016-10-04 18:47:23 +0300

    Create a concrete List collection class (ArrayList and LinkedList), and inject it into bean property.

---
+++ SpringCollectionFactoryBean/src/main/java/github/tiMorpheus/common/App.java
@@ -0,0 +1,17 @@
+package github.tiMorpheus.common;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("SpringBeans.xml");
+
+        Customer customer = (Customer) context.getBean("CustomerBean");
+        System.out.println(customer);
+
+    }
+}
---
+++ SpringCollectionFactoryBean/src/main/java/github/tiMorpheus/common/Customer.java
@@ -0,0 +1,23 @@
+package github.tiMorpheus.common;
+
+import java.util.List;
+
+public class Customer
+{
+    private List lists;
+
+    public List getLists() {
+        return lists;
+    }
+
+    public void setLists(List lists) {
+        this.lists = lists;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "lists=" + lists +
+                +'}';
+    }
+}
