f342126f4e332737ee983f95151f235e657cb3bf
Tima Tolochko <timur10645@icloud.com>
2016-10-05 23:17:20 +0300

    Perform certain actions upon bean initialization and destruction. (interface)

---
+++ SpringInitBeanDisposable/src/main/java/github/tiMorpheus/services/App.java
@@ -0,0 +1,17 @@
+package github.tiMorpheus.services;
+
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context =
+                new ClassPathXmlApplicationContext(new String[] {"Spring-Customer.xml"});
+
+        CustomerService service = (CustomerService) context.getBean("customerService");
+
+        System.out.println(service);
+
+        context.close();
+    }
+}
